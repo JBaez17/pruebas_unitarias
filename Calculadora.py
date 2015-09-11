2d5092f6e37629e6bf0badf2043bd46dcb520b49
JBaez17 <kirito_baz@outlook.com>
2015-09-11 11:44:49 -0500

    Se separo la funcionalidad del codigo de la prueba, se guardo en el archivo Calculadora.py y fue agregado al repositorio

---
+++ Calculadora.py
@@ -0,0 +1,8 @@
+class Calculadora():
+    #Se puede poner "pass" para indicar al compilador que esta correcto porque aun no lo implementas
+    def suma(self,num1,num2):
+        #En consola el "." significa que la prueba paso, "F" significa que la prueba fallo
+        return num1 + num2
+
+    def resta(self, num1, num2):
+        return num1 - num2
