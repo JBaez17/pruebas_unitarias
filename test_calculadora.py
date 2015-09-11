b6025c59f9a7de7414ab339226c36f70750bae32
JBaez17 <kirito_baz@outlook.com>
2015-09-11 11:17:47 -0500

    Se agregaron 2 pruebas ms y el metodo setUp, y se refactorizo el codigo

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,10 +1,30 @@
 import unittest
 
 class Test(unittest.TestCase):
+
+    #setup, es el primer metodo que se ejecuta
+    #tearDown, es el ultimo metodo que se ejecuta
+    def setUp(self):
+        self.calc = Calculadora()
+
     def test_suma_de_2_mas_2(self):
-        calc = Calculadora()
-        resultado = calc.suma(2,2)
+        resultado = self.calc.suma(2,2)
         self.assertEqual(4, resultado)
+
+    def test_suma_de_3_mas_3(self):
+        resultado = self.calc.suma(3,3)
+        self.assertEqual(6, resultado)
+
+    def test_suma_de_5_mas_5(self):
+        resultado = self.calc.suma(5,5)
+        self.assertEqual(10, resultado)
+
+
+class Calculadora():
+    #Se puede poner "pass" para indicar al compilador que esta correcto porque aun no lo implementas
+    def suma(self,num1,num2):
+        #En consola el "." significa que la prueba paso, "F" significa que la prueba fallo
+        return num1 + num2
 
 if __name__=='__main__':
     unittest.main()
